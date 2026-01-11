d6210a1a6bb57c12ba6bc5f6d44f06a6a8cd12ac
MF1DD <github@mf1dd.com>
2026-01-11 22:18:20 +0100

    Remove psr7 and use slim psr7

--- src/App/UserInterface/Web/Adapter/ResponseAdapter.php
+++ src/App/UserInterface/Web/Adapter/ResponseAdapter.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\UserInterface\Web\Adapter;
 
-use GuzzleHttp\Psr7\Response;
+use Slim\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Utils\PHPStan\Attribute\NoTestNeeded;
